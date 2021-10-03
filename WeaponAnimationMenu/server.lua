85addf6267a3415ae957bf210ee3aa8b8a11e84e
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2021-10-03 22:50:15 +0200

    Delete WeaponAnimationMenu directory

--- WeaponAnimationMenu/server.lua
+++
@@ -1,8 +0,0 @@
-if GetCurrentResourceName() ~= "WeaponAnimationMenu" then
-    print("Error: Change the resource name to \"WeaponAnimationMenu\" or it won't work!")
-end
-
--- open menu.
-RegisterCommand(config.openCommand, function(source, args, rawCommand)
-    TriggerClientEvent('Andyyy:OpenWAM', source)
-end, false)
